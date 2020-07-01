74ab1c277ccef7ac619398b507b08bea679e0739
Priyaa Sahasranaman <priyaa.sahasranaman@in.abb.com>
2020-07-01 14:53:18 +0530

    tabs

--- shell-app/src/app/Shared/content/content.component.ts
+++ shell-app/src/app/Shared/content/content.component.ts
@@ -10,24 +10,26 @@ import { NavService } from '../Services/nav.service';
   encapsulation: ViewEncapsulation.None
 })
 export class ContentComponent implements OnInit {
+  title = 'metaR';
 
+  tabs = [];
   constructor(private navService: NavService) { }
 
   ngOnInit(): void {
     this.navService.selectedMenuItem.subscribe((data) => this.addTab(data))
   }
 
-  title = 'metaR';
-
-  tabs = [];
+ 
   selected = new FormControl(0);
 
   addTab(tabName: string, selectAfterAdding= true) {
-    this.tabs.push(tabName);
-
-    if (selectAfterAdding) {
+    //styling
+    if(this.tabs && this.tabs.indexOf(tabName) < 0){
+      this.tabs.push(tabName);
       this.selected.setValue(this.tabs.length - 1);
     }
+    else
+      this.selected.setValue(this.tabs.indexOf(tabName));
   }
 
   removeTab(index: number) {
