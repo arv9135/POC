8a1e787a2af63eb0077e5749be91dabb0df24310
arvind singh <arv9135@outlook.com>
2020-07-11 22:48:19 +0530

    Web worker added.

--- POC.Web/ClientApp/src/app/app.component.ts
+++ POC.Web/ClientApp/src/app/app.component.ts
@@ -56,3 +56,18 @@ export class AppComponent {
   }
 
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const sharedWorker = new SharedWorker('./app.worker', { type: 'module' })
+
+
+  const worker = new Worker('./app.worker', { type: 'module' });
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
